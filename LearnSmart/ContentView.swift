0e5e3ee74cc12deb5c62b0d8dcc3d100536001b6
Thang <llsogekingll@gmail.com>
2020-01-17 16:40:08 +1100

    Test git

--- LearnSmart/ContentView.swift
+++ LearnSmart/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack(alignment: .center) {
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Take a Photo")
+                    }
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Browse a Photo")
+                    }
+                }.padding()
+                Spacer()
+            }.padding(.top, 50).padding(.leading, 20).padding(.trailing, 20)
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            Text("Placeholder")
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            Text("Placeholder")
+        }
     }
 }
 
